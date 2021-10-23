e44967125964fe90938c4c294a08d72c64580918
blinkspark <3829294+blinkspark@users.noreply.github.com>
2021-10-23 21:28:22 +0800

    filestore basic

---
+++ filestore-fe/components/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="panel is-primary">
+    <div class="panel-heading">Login</div>
+    <div class="panel-block">
+      <form @submit.prevent="login">
+        <div class="field">
+          <label class="label">Email</label>
+          <div class="control">
+            <input
+              class="input"
+              type="email"
+              v-model="email"
+              placeholder="Email"
+            />
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Password</label>
+          <div class="control">
+            <input
+              class="input"
+              type="password"
+              v-model="password"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <button class="button is-primary">Login</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.email, this.password);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ filestore-fe/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="is-size-1">Hello World!</h1>
+    <p>This is my first Vue.js project.</p>
+    <nuxt-link class="button is-danger" to="/login">login</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
