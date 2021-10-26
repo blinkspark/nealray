2fa90e2c32018ffcbcdfb0c782118f3697d3dac1
blinkspark <3829294+blinkspark@users.noreply.github.com>
2021-10-26 19:55:53 +0800

    dummy file panel

--- filestore-fe/pages/index.vue
+++ filestore-fe/pages/index.vue
@@ -1,29 +1,33 @@
 <template>
   <div>
     <div class="panel is-primary">
-      <div class="panel-heading m-0">
-        Heading
+      <div class="panel-heading m-0">Heading</div>
+      <div class="panel-block">
+        <nav class="breadcrumb is-right" aria-label="breadcrumbs">
+          <ul>
+            <li><a href="#">Bulma</a></li>
+            <li><a href="#">Documentation</a></li>
+            <li><a href="#">Components</a></li>
+            <li class="is-active">
+              <a href="#" aria-current="page">Breadcrumb</a>
+            </li>
+          </ul>
+        </nav>
       </div>
       <div class="panel-block">
-        <div class="columns">
-          <div class="column is-half">
-            <div class="field">
-              <label class="label">Label</label>
-              <div class="control">
-                <input class="input" type="text" placeholder="Text input">
-              </div>
-            </div>
-          </div>
-          <div class="column is-half">
-            <div class="field">
-              <label class="label">Label</label>
-              <div class="control">
-                <input class="input" type="text" placeholder="Text input">
-              </div>
-            </div>
-          </div>
-        </div>
+        <p class="control has-icons-left">
+          <input class="input is-primary" type="text" placeholder="Search" />
+          <span class="icon is-small is-left">
+            <i class="fas fa-search"></i>
+          </span>
+        </p>
       </div>
+      <a class="panel-block pl-6">
+        <span class="panel-icon">
+          <i class="fas fa-file-alt" aria-hidden="true"></i>
+        </span>
+        marksheet
+      </a>
     </div>
   </div>
 </template>
@@ -31,9 +35,3 @@
 <script>
 export default {};
 </script>
-
-<style lang="scss">
-body {
-  // background-color: #000;
-}
-</style>
